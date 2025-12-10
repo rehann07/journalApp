081e663c1a898368100a94278e8821f79acc6d3e
Rehan Naikwadi <rehannaikwadi07@gmail.com>
2025-12-10 13:16:36 +0530

    adds jwt

--- src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -19,13 +19,27 @@ public class PublicController {
         return  "Ok";
     }
 
-    @PostMapping("/create-user")
-    public ResponseEntity<User> createUser(@RequestBody User user){
+    @PostMapping("/signup")
+    public ResponseEntity<User> signUp(@RequestBody User user){
         try {
             userService.registerUser(user);
             return new ResponseEntity<>(user, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user){
+        try{
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Exception occurred while createAuthenticationToken ", e);
+            return new ResponseEntity<>("Error : "+ e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
